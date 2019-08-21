39426aee1e05857fafc6ad0260d056dc8a4283ab
Thomas Johander <thomas.johnader@target.com>
2019-08-21 10:53:21 -0500

    docker-compose example is working

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,9 +28,11 @@ if (process.env.DATABASE_URL) {
   };
 } else {
   config = {
-    host: 'localhost', // Server hosting the postgres database
+    host: 'db', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-    database: 'prime_app', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    user: 'postgres',
+    password: 'primeordeal',
+    database: 'prime-app', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
